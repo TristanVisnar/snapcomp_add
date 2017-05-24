7f8174c3b100ec7ec8545e3be78a9e652f7187fa
TV\Tristan <tristan.visnar@student.um.si>
2017-05-24 19:06:50 +0200

    Generira_ppm

--- programi/napolniBazo/generiraj.cpp
+++ programi/napolniBazo/generiraj.cpp
@@ -1,9 +1,43 @@
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <time.h>
+#include <stdlib.h>
 
 using namespace std;
 
-int main() {
-	cout << "hello world";
-}
+const int width = 600, height = 400;
 
+int main(int argc, char ** argv) {
+	if (argc != 2) {
+		cout << "Napaka klicu aplikacije" << endl;
+		system("pause");
+		return -1;
+	}
+
+	for (int i = 0; i < stoi(argv[1]); i++) {
+		srand(i*(1484011024*(18+i))% 4294967295);
+		stringstream ss;
+		ss << "ppm/picture" << i << ".ppm";
+		string ime = ss.str();
+		ofstream img(ime);
+		img << "P3" << endl;
+		img << width << " " << height << endl;
+		img << "255" << endl;
+
+		for (int y = 0; y < height; y++) {
+			for (int x = 0; x < width; x++) {
+				int r = rand() % 255;
+				int g = rand() % 255;
+				int b = rand() % 255;
+
+				img << r << " " << g << " " << b << endl;
+			}
+		}
+		img.close();
+	}
+	system("pause");
+	return 0;
+}
